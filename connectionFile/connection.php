93aece22490dc6e4f9ea2c5c2d07a140d818a42c
Andrija Pavlovic <andrija.pavlovic996@gmail.com>
2017-10-07 18:57:23 +0200

    Parts folder created

---
+++ connectionFile/connection.php
@@ -0,0 +1,13 @@
+<?php 
+	
+	$host = "localhost";
+	$root = "root";
+	$pass = "";
+	$db = "izlasci";
+	$conn= mysqli_connect($host,$root,$pass,$db);
+	if(!$conn){
+		echo "Connection problem: " . mysqli_connect_errno() . PHP_EOL();;
+	}
+	
+
+ ?>
---
+++ connectionFile/define.php
@@ -0,0 +1,7 @@
+<?php 
+
+	define("EMAIL","andrija.pavlovic996@gmail.com");
+	define("NAME","jpstore.com");
+	define("USERNAME","andrija.pavlovic996@gmail.com");
+	define("PASSWORD","Mateja1703");
+ ?>
