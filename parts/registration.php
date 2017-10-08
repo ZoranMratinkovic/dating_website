314b7a349eec92ca4054d6159b9ea7ed3a5ed70e
zoki <zoran3001@gmail.com>
2017-10-08 20:00:44 +0200

    zoki

--- parts/registration.php
+++ parts/registration.php
@@ -1,42 +1,149 @@
+      <?php include("connectionFile/connection.php");?>
+    <script type="text/javascript">
+    function posalji() {
+      alert('radii');
+var email = document.getElementById('reg_email').value;
+var ime = document.getElementById('reg_first').value;
+var user = document.getElementById('reg_user').value;
+var pass=document.getElementById('reg_pass').value;
+var pass1=document.getElementById('reg_pass1').value;
+var greske= new Array();
+var sadrzaj=new Array();
+  var greskeID = new Array();
+var reime= /^[a-z]{2,14}$/;
+var reemail= /^(\w+[\-\.])*\w+@(\w+\.)+[A-Za-z]+$/;
+var repass= /^[a-z]{2,14}$/;
+var reuser= /^[a-z]{2,14}$/;
+
+if(reime.test(ime))
+	{
+		sadrzaj.push(ime);
+
+
+	}
+	else
+
+	{
+
+		greske.push('erorr');
+
+	}
+  if(reuser.test(user))
+  	{
+  		sadrzaj.push(user)
+
+
+  	}
+  	else
+
+  	{
+
+  			greske.push('erorr');
+
+  	}
+	if(reemail.test(email))
+	{
+		sadrzaj.push(email);
+
+	}
+	else
+	{
+		greske.push('erorr');
+
+	}
+
+  if(pass==pass1){
+      sadrzaj.push(pass)
+  }
+  else{
+    greske.push('error')
+  }
+	if(repass.test(pass))
+	{
+		sadrzaj.push(pass)
+
+	}
+	else
+	{
+			greske.push('erorr');
+
+	}
+  if()
+
+
+
+if(sadrzaj.length == 4)
+	{
+		alert("Vaša poruka je poslata,odgovor ce biti najkasnije sledečeg radnog dana")
+
+}
+else {alert('ima greske!!!')}
+}
+
+    </script>
     <div class="register_form_inner zoom-anim-dialog mfp-hide" id="register_form">
             <div class="row">
                 <div class="col-md-6">
                     <div class="registration_man">
-                        <img src="img/Registration_man.png" alt="">
+                        <img src="img/girl.jpg" alt="">
                     </div>
                 </div>
                 <div class="col-md-6">
                     <div class="registration_form_s">
                         <h4>Registration</h4>
-                        <form>
+                        <form class="" action="#" method="post" >
+
+
                             <div class="form-group">
-                                <input type="email" class="form-control" id="reg_email" placeholder="Email">
+                                <input type="email" class="form-control" id="reg_email" placeholder="Email" name="email" required >
                             </div>
                             <div class="form-group">
-                                <input type="text" class="form-control" id="reg_first" placeholder="Full Name">
+                                <input type="text" class="form-control" id="reg_first" placeholder="Full Name" name="fullname" required>
                             </div>
                             <div class="form-group">
-                                <input type="text" class="form-control" id="reg_user" placeholder="Username">
+                                <input type="text" class="form-control" id="reg_user" placeholder="Username" name="username" required>
                             </div>
                             <div class="form-group">
-                                <input type="password" class="form-control" id="reg_pass" placeholder="Password">
+                                <input type="password" class="form-control" id="reg_pass" placeholder="Password" name="password" required>
+                            </div>
+                            <div class="form-group">
+                                <input type="password" class="form-control" id="reg_pass1" placeholder="again password" name="password1" required>
                             </div>
                             <div class="form-group">
                                 <div class="btn-group">
-                                    <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown">
-                                    <span data-bind="label">Gender</span>&nbsp;<span class="arrow_carrot-down"><i class="fa fa-sort-asc" aria-hidden="true"></i><i class="fa fa-sort-desc" aria-hidden="true"></i></span>
-                                    </button>
-                                    <ul class="dropdown-menu" role="menu">
-                                        <li><a href="#">Male</a></li>
-                                        <li><a href="#">Female</a></li>
-                                    </ul>
+
+                                    <select class="form-control" name="gender">
+                                      <option value="0">Geschlecht</option>
+                                      <option value="Mann">Mann</option>
+                                      <option value="Frau">Frau</option>
+                                        <option value="Anderes">Anderes</option>
+                                    </select>
                                 </div>
                             </div>
                             <div class="form-group">
-                                <div class="datepicker">
-                                    <input type='text' class="form-control datetimepicker4" placeholder="Birthday" />
-                                    <span class="add-on"><i class="fa fa-calendar" aria-hidden="true"></i></span>
-                                </div>
+                              <?php
+
+                                                        echo '<select name="monat" class="trecina form-control">';
+                                                          echo '<option>Monat</option>';
+                                                          for($i = 1; $i <= 12; $i++){
+                                                            $i = str_pad($i, 2, 0, STR_PAD_LEFT);
+                                                            echo "<option value='$i'>$i</option>";
+                                                          }
+                                                        echo '</select>';
+                                                        echo '<select name="tag" class="trecina form-control">';
+                                                          echo '<option>Tag</option>';
+                                                          for($i = 1; $i <= 31; $i++){
+                                                            $i = str_pad($i, 2, 0, STR_PAD_LEFT);
+                                                            echo "<option value='$i'>$i</option>";
+                                                          }
+                                                        echo '</select>';
+                                                        echo '<select name="jahr" class="trecina form-control">';
+                                                          echo '<option>Jahr</option>';
+                                                          for($i = date('Y'); $i >= date('Y', strtotime('-100 years')); $i--){
+                                                            echo "<option value='$i'>$i</option>";
+                                                          }
+                                                        echo '</select>';
+?>
                             </div>
                             <div class="reg_chose form-group">
                                 <div class="reg_check_box">
@@ -44,11 +151,27 @@
                                     <label for="s-option">I`m Not Robot</label>
                                     <div class="check"><div class="inside"></div></div>
                                 </div>
-                                <button type="submit" value="LogIn" class="btn form-control login_btn">Register</button>
+                                <button type="button" value="LogIn" class="btn form-control login_btn" name="registeru" onclick="posalji();">Register</button>
                             </div>
+
                         </form>
                         <img class="mfp-close" src="img/close-btn.png" alt="">
                     </div>
                 </div>
             </div>
         </div>
+        <?php/*
+        if(isset($_REQUEST['registeru'])){
+        $email=$_POST['email'];
+        $fullname=$_POST['fullname'];
+        $username=$_POST['username'];
+        $password=$_POST['password'];
+        $password1=$_POST['password1'];
+        $geschlecht=$_POST['gender'];
+        $monat=$_REQUEST['monat'];
+        $tag=$_REQUEST['tag'];
+        $jahr=$_REQUEST['jahr'];
+          $upit="INSERT INTO user VALUES('','$email','$username','$password','$geschlecht',$tag,$monat,$jahr)";
+          $rezupit=$conn->query($upit)or die('losee');
+        }*/
+          ?>
