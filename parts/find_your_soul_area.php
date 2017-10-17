1e996c96c8553a376dac64118a642c701c561e80
Andrija Pavlovic <andrija.pavlovic996@gmail.com>
2017-10-18 01:26:17 +0200

    Portrait responsive done

--- parts/find_your_soul_area.php
+++ parts/find_your_soul_area.php
@@ -5,21 +5,21 @@
                     <img src="img/w-title-b.png" alt="">
                 </div>
                 <div class="row">
-                    <div class="col-md-4">
+                    <div class="col-md-4 col-sm-4">
                         <div class="find_soul_item">
                             <img src="img/soul-icon/soul-1.png" alt="">
                             <h4>Profil erstellen</h4>
                             <p>Erstellen sie ein Profil als user oder anbieter und profietieren sie.</p>
                         </div>
                     </div>
-                    <div class="col-md-4">
+                    <div class="col-md-4 col-sm-4">
                         <div class="find_soul_item">
                             <img src="img/soul-icon/soul-2.png" alt="">
                             <h4>Finden sie was sie wollen! </h4>
                             <p>Leicht zu durchsuchen viele kategorien viele angebote</p>
                         </div>
                     </div>
-                    <div class="col-md-4">
+                    <div class="col-md-4 col-sm-4">
                         <div class="find_soul_item">
                             <img src="img/soul-icon/soul-3.png" alt="">
                             <h4>Treffen sie sich</h4>
